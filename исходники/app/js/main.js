14622a430f87c0ad2de70d635072bbbb9fc6c522
Наталья <0207natalya@gmail.com>
2023-05-18 07:07:52 +0600

    sources

---
+++ исходники/app/js/main.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let form = document.getElementById('myForm');
+    let popup = document.getElementById('myPopup');
+    let overlay = document.getElementById('overlay');
+    let closeBtn = document.getElementById('closeBtn');
+    let body = document.getElementsByTagName('body')[0];
+
+    form.addEventListener('submit', function(event) {
+      event.preventDefault(); 
+      popup.style.display = 'block';
+      overlay.style.display = 'block';
+      body.style.overflow = 'hidden';
+      popup.focus();
+    });
+
+    closeBtn.addEventListener('click', function() {
+      popup.style.display = 'none';
+      overlay.style.display = 'none';
+      body.style.overflow = 'auto';
+    });
+
+    overlay.addEventListener('click', function() {
+      popup.style.display = 'none';
+      overlay.style.display = 'none';
+      body.style.overflow = 'auto';
+    });
+  });
+
+  const burgerMenu = document.querySelector('.burger__menu')
+  const menu = document.querySelector('.header__right')
+
+  burgerMenu.addEventListener('click', () => {
+    menu.classList.toggle('active')
+    burgerMenu.classList.toggle('active')
+  })
